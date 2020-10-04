23379e60cd134515aa65bf409f03ffea4930ec52
llavieri <llavieri@csisoft.com>
2020-10-04 16:38:11 -0400

    added unit tests

--- HackerNewsTaskTests/Controllers/HackerNewsControllerTests.cs
+++ HackerNewsTaskTests/Controllers/HackerNewsControllerTests.cs
@@ -1,89 +1,98 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using HackerNewsTaskTests.Controllers;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.TestHost;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using System;
-using System.IO;
-using Microsoft.Extensions.Caching.Memory;
-using Microsoft.Extensions.DependencyInjection;
-using NSubstitute;
 
 namespace HackerNewsTask.Controllers.Tests
 {
     [TestClass()]
     public class HackerNewsControllerTests
     {
-        private static readonly Uri BaseUri = new Uri("http://localhost:35072/");
-
-        [TestMethod()]
-        public void HackerNewsFetchFirstPage()
+        private static async Task<HttpClient> Init()
         {
-            var httpResult = HttpHelper.Get(new Uri(BaseUri, "api/hackernews/fetch?pageIndex=0&pageSize=50"));
-
-            var des = JsonConvert.DeserializeObject(httpResult);
-
-            var jObj = des as JObject;
-
-            Assert.IsTrue(jObj["results"] is JArray jArr && jArr.Count <= 50);
+            var _webhost = WebHost.CreateDefaultBuilder()
+                .UseStartup<Startup>()
+                .UseUrls("http://localhost:35072")
+                .UseTestServer()
+                .Build();
+            await _webhost.StartAsync();
+            return _webhost.GetTestServer().CreateClient();
         }
 
+        /// <summary>
+        /// Simple query
+        /// </summary>
         [TestMethod()]
-        [Timeout(50000)]
-        [ExpectedException(typeof(Exception), "oh no!")]
-        public void HackerNewsFetchBigPage()
+        public void Simple_Query()
         {
-            HttpHelper.Get(new Uri(BaseUri, "api/hackernews/fetch?pageIndex=5000&pageSize=10000"));
+            var client = Init();
+            var response = client.Result.GetAsync("/api/hackernews/fetch?pageIndex=0&pageSize=50").Result;
+            Assert.IsTrue(response.IsSuccessStatusCode);
         }
 
-
-        //[TestMethod()]
-        //public void asda()
-        //{
-        //    // Arrange
-        //    var myDependency = Substitute.For<IMemoryCache>();
-        //    var webHostBuilder = WebHostBuilderFactory.Create<Startup>(
-        //        services => services.Replace<IMyDependency>(myDependency), 
-        //        services => services.SetSystemClock(DateTime.Now.AddYears(-2)));
-        //    var serviceProvider = webHostBuilder.Build().Services;
-        //    var myService = serviceProvider.GetRequiredService<IMyService>();
-        //    var expected = "awesome stuff";
-
-        //    // Act
-        //    var actual = myService.DoStuff();
-
-        //    // Assert
-        //    Assert.That(actual, Is.EqualTo(expected));
-        //}
-    }
-
-    internal static class WebHostBuilderFactory
-    {
-        public static IWebHostBuilder Create<TStartup>(
-            params Action<IServiceCollection>[] testSpecificServiceConfigurations)
-            where TStartup : class
+        /// <summary>
+        /// Simple Search Query
+        /// </summary>
+        [TestMethod()]
+        public void Simple_Search_Query()
         {
-            var contentRootPath = Path.Combine(Directory.GetCurrentDirectory(), @"..\..\src\App");
-            return new WebHostBuilder().UseContentRoot(contentRootPath)
-                .UseEnvironment(EnvironmentName.Development)
-                .UseStartup<TStartup>()
-                .ConfigureServices(services =>
-                {
-                    services.AddCors(options =>
-                    {
-                        options.AddPolicy("CorsPolicy",
-                            builder => builder.AllowAnyOrigin()
-                                .AllowAnyMethod()
-                                .AllowAnyHeader());
-                    });
-                    services.AddMemoryCache();
-                    services.AddControllers().AddNewtonsoftJson(options =>
-                        options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
-                    );
-                });
+            var client = Init();
+            var response = client.Result.GetAsync("/api/hackernews/fetch?pageIndex=0&pageSize=50&Search=VPN").Result;
+            Assert.IsTrue(response.IsSuccessStatusCode);
+        }
 
+        /// <summary>
+        /// Number of results need to be less or equal to the pageSize parameter
+        /// </summary>
+        [TestMethod()]
+        public void Correct_Number_Of_Results()
+        {
+            var client = Init();
+            var response = client.Result.GetAsync("/api/hackernews/fetch?pageIndex=0&pageSize=50&Search=VPN").Result;
+            var s = response.Content.ReadAsStringAsync().Result;
+            var deserialized = JsonConvert.DeserializeObject(s);
+            var jObj = deserialized as JObject;
+            Assert.IsTrue(jObj["results"] != null && jObj["results"] is JArray arr && arr.Count <= 50); 
+        }
+
+        /// <summary>
+        /// If its too big of page, internal server error
+        /// </summary>
+        [TestMethod()]
+        public void Big_Parameters_Internal_Server_Error()
+        {
+            var client = Init();
+            var response = client.Result.GetAsync("/api/hackernews/fetch?pageIndex=0&pageSize=9999999999&Search=VPN").Result;
+            Assert.IsTrue(!response.IsSuccessStatusCode); // internal server error
+        }
+
+        /// <summary>
+        /// load simple item
+        /// </summary>
+        [TestMethod()]
+        public void Loaded_Item_Has_A_Title()
+        {
+            var client = Init();
+            var response = client.Result.GetAsync("/api/hackernews/load?id=24678381").Result;
+            var s = response.Content.ReadAsStringAsync().Result;
+            var deserialized = JsonConvert.DeserializeObject(s);
+            var jObj = deserialized as JObject;
+            Assert.IsTrue(jObj["title"] != null);//contains title
+        }
+
+        /// <summary>
+        /// Unexisting item, internal server error
+        /// </summary>
+        [TestMethod()]
+        public void Item_Does_Not_Exist()
+        {
+            var client = Init();
+            var response = client.Result.GetAsync("/api/hackernews/load?id=99999999").Result;
+            Assert.IsTrue(!response.IsSuccessStatusCode); // internal server error
         }
     }
 }
