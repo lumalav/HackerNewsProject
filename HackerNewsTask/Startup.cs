65268332187f272c00db68305d06ee6e63103bca
llavieri <llavieri@csisoft.com>
2020-10-04 11:06:18 -0400

    removed unused imports in startup.cs

--- HackerNewsTask/Startup.cs
+++ HackerNewsTask/Startup.cs
@@ -1,8 +1,5 @@
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
